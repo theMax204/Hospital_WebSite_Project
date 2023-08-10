3dc9387cd653f2bd2419ace3fff7c945d32a0b09
theMax204 <maxi201498@gmail.com>
2023-08-10 13:56:53 -0600

    comint test

--- ProyectoWebhospital/registros.php
+++ ProyectoWebhospital/registros.php
@@ -20,7 +20,7 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link href="https://kit.fontawesome.com/97cef9f55a.js" crossorigin="anonymous" rel="stylesheet">
 
-	<!-- JS -->
+	<!-- JavaScript -->
 	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
