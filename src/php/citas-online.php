9b439c7139077d297356b52d3fc817426bd126c9
kendevtech <kendallsalazarsoto@gmail.com>
2023-08-10 20:58:53 -0600

    Se actualizaron las clases index.html y mapa.html. Se hicieron responsivas y además se ajusto el contenido de ambas

---
+++ src/php/citas-online.php
@@ -0,0 +1,184 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Citas</title>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/main.css">
+  <link rel="stylesheet" href="css/generalh.css">
+  <link rel="stylesheet" href="css/citas-online.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+</head>
+
+<body>
+  <header>
+    <nav class="navbar navbar-expand-lg bg-colorarriba">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">
+          <img class="logoprinc" src="img/logo.png" alt="icon">
+        </a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav menu">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="index.php">Principal</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="Citas en linea.php">Citas en línea</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="Mapa.php">Mapa</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="registros.php">Registrarse</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+
+    <div class="container mt-5">
+      <div id="carouselExample" class="carousel slide" data-bs-ride="carousel" data-bs-interval="2300">
+        <div class="carousel-inner">
+          <div class="carousel-item active">
+            <img src="img/Imagenhospital1.jpg" class="imgcarusel" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="img/Imagenhospital2.jpg" class="imgcarusel" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="img/Imagenhospital3.jpg" class="imgcarusel" alt="...">
+          </div>
+          <div class="carousel-item">
+            <img src="img/Imagenhospital4.jpg" class="imgcarusel" alt="...">
+          </div>
+        </div>
+        <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Previous</span>
+        </button>
+        <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+          <span class="visually-hidden">Next</span>
+        </button>
+      </div>
+
+      <div class="card-base mt-4">
+        <div class="card-body titulomain-card">
+          <div class="titulosecondmain card-body">
+            <h2 class="tituloprincipal">Encuentre aquí el especialista de su preferencia</h2>
+            <p class="tituloservicios">Todos los servicios disponibles</p>
+            <form class="d-flex" role="search" onsubmit="submitForm(event)">
+              <input id="buscador" class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+              <button class="btn btn-outline-success" type="submit">Buscar</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+  <!-- Contenido -->
+  <div class="card-citas">
+    <div class="container" style="margin-top: 100px;">
+      <section class="row justify-content-center">
+        <div class="col-md-6 col-lg-7">
+          <form class="texto-modificado" action="">
+            <div class="row mb-3">
+              <div class="col-md-6">
+                <div class="form-floating">
+                  <input type="text" class="form-control" id="nombre" placeholder="Nombre">
+                  <label for="nombre">Nombre</label>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="form-floating">
+                  <input type="text" class="form-control" id="apellido" placeholder="Apellidos">
+                  <label for="apellido">Apellidos</label>
+                </div>
+              </div>
+            </div>
+            <div class="row mb-3">
+              <div class="col-md-6">
+                <div class="form-floating">
+                  <input type="text" class="form-control" id="cedula" placeholder="Cédula">
+                  <label for="cedula">Cédula</label>
+                </div>
+              </div>
+              <div class="col-md-6">
+                <div class="form-floating">
+                  <input type="text" class="form-control" id="telefono" placeholder="Teléfono">
+                  <label for="telefono">Teléfono</label>
+                </div>
+              </div>
+            </div>
+            <div class="form-floating mb-3">
+              <input type="email" class="form-control" id="email" placeholder="Email">
+              <label for="email">Email</label>
+            </div>
+            <div class="mb-3">
+              <select class="form-select" aria-label="Servicio deseado">
+                <option value="" disabled selected>Servicio deseado:</option>
+                <option value="1">Cardiologia</option>
+                <option value="2">Dermatologia</option>
+                <option value="3">Departamento De Emergencias</option>
+                <option value="4">Endocrinologia</option>
+                <option value="5">Farmacia</option>
+                <option value="6">Fisiatria</option>
+                <option value="7">Gastroenterologia</option>
+                <option value="8">Geriatria</option>
+                <option value="9">Ginecologia</option>
+                <option value="10">Medicina General</option>
+                <option value="11">Medicina Interna</option>
+                <option value="12">Nutrición</option>
+                <option value="13">Odontologia</option>
+                <option value="14">Oftalmologia</option>
+                <option value="15">Oncologia</option>
+                <option value="16">Optometria</option>
+                <option value="17">Ortopedia y Traumatologia</option>
+                <option value="18">Otorrinolaringologia</option>
+                <option value="19">Otros</option>
+                <option value="20">Pediatria</option>
+                <option value="21">Psiquiatra y Psicologia</option>
+                <option value="22">Radiologia</option>
+                <option value="23">Reumatologia</option>
+                <option value="24">Terapia Fisica</option>
+                <option value="25">Urologia</option>
+                <option value="26">Vascular Periferico</option>
+              </select>
+            </div>
+            <div class="mb-3">
+              <select class="form-select" aria-label="Sede de preferencia">
+                <option value="" disabled selected>Seleccione la sede de preferencia:</option>
+                <option value="1">Sede 1</option>
+                <option value="2">Sede 2</option>
+                <option value="3">Sede 3</option>
+              </select>
+            </div>
+            <div class="mb-3">
+              <textarea placeholder="Asunto" class="form-control" name="mensaje" id="mensaje" cols="30" rows="10"></textarea>
+            </div>
+            <div class="mb-3">
+              <button type="submit" class="btn btn-primary">Enviar</button>
+            </div>
+          </form>
+        </div>
+      </section>
+    </div>
+  </div>
+  <!-- Footer -->
+  <?php include "footer.php" ?>
+  <script src="js/vendor/modernizr-3.11.2.min.js"></script>
+  <script src="js/plugins.js"></script>
+  <script src="js/main.js"></script>
+  <script src="https://kit.fontawesome.com/97cef9f55a.js" crossorigin="anonymous"></script>
+  <!-- archivos js adicionales -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
+  <script src="js/principal.js"></script>
+</body>
+
+</html>
--- src/php/registros.php
+++ src/php/registros.php
@@ -2,63 +2,111 @@
 <html lang="es">
 
 <head>
-	<meta charset="UTF-8">
-	<title>Registro y Login</title>
+    <meta charset="UTF-8">
+    <title>Registro y Login</title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta property="og:title" content="">
     <meta property="og:type" content="">
     <meta property="og:url" content="">
     <meta property="og:image" content="">
-
-	<!-- CSS -->
-	<link rel="stylesheet" href="css/normalize.css">
-    <link rel="stylesheet" href="css/main.css">
-    <link rel="stylesheet" href="css/generalh.css">
-    <link rel="stylesheet" href="css/registros.css">
-    <link rel="stylesheet" href="css/procesarRegistro.css">
+    <!-- CSS -->
+    <link rel="stylesheet" href="css/normalize.css">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
     <link href="https://kit.fontawesome.com/97cef9f55a.js" crossorigin="anonymous" rel="stylesheet">
-
-	<!-- JavaScript -->
-	<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
-    <script src="js/main.js"></script>
-    <script src="js/principal.js"></script>
 </head>
 
 <body class="bodyprincipal">
+    <!-- Header -->
+    <header>
+        <nav class="navbar navbar-expand-lg bg-colorarriba">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img class="logoprinc" src="img/logo.png" alt="icon">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav menu">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="index.php">Principal</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="Citas en linea.php">Citas en línea</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="Mapa.php">Mapa</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="registros.php">Registrarse</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
 
-	<!-- Header -->
-	<?php
-    $currentPage = basename($_SERVER['PHP_SELF']);
-    include "headerdos.php";
-    ?>
-
-	<section class="container">
-        <div class="containerregistro">
-            <div class="contenidologin">
-                <div class="containerreg">
-                    <div class="tabsreg">
-                        <div class="tabreg uiregister active" data-tab="tabreg-login">Login</div>
-                        <div class="tabreg uiregister" data-tab="tabreg-register">Register</div>
+        <div class="container mt-5">
+            <div id="carouselExample" class="carousel slide" data-bs-ride="carousel" data-bs-interval="2300">
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="img/Imagenhospital1.jpg" class="imgcarusel" alt="...">
                     </div>
+                    <div class="carousel-item">
+                        <img src="img/Imagenhospital2.jpg" class="imgcarusel" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="img/Imagenhospital3.jpg" class="imgcarusel" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="img/Imagenhospital4.jpg" class="imgcarusel" alt="...">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+            </div>
 
-                    <!-- Login form -->
-                    <form id="tabreg-login" class="formaregistro uiregister active">
-                        <h2 class="mb-4">Login</h2>
+            <div class="card-base mt-4">
+                <div class="card-body titulomain-card">
+                    <div class="titulosecondmain card-body">
+                        <h2 class="tituloprincipal">Encuentre aquí el especialista de su preferencia</h2>
+                        <p class="tituloservicios">Todos los servicios disponibles</p>
+                        <form class="d-flex" role="search" onsubmit="submitForm(event)">
+                            <input id="buscador" class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                            <button class="btn btn-outline-success" type="submit">Buscar</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <section class="container">
+        <div class="container-registro">
+            <div class="contenido-login">
+                <div class="container-reg">
+                    <div class="tabs-reg">
+                        <div class="tab-reg ui-register active" data-tab="tab-login">Iniciar Sesión</div>
+                        <div class="tab-reg ui-register" data-tab="tab-registrar">Registrarse</div>
+                    </div>
+                    <!-- Formulario de inicio de sesión -->
+                    <form id="tab-login" class="formulario-registro ui-register active">
+                        <h2 class="mb-4">Iniciar Sesión</h2>
                         <div class="mb-3">
-                            <label for="loginUsername" class="form-label">Username</label>
-                            <input type="text" id="loginUsername" class="uiregister form-control" placeholder="Username">
+                            <label for="login-username" class="form-label">Nombre de Usuario</label>
+                            <input type="text" id="login-username" class="ui-register form-control" placeholder="Nombre de Usuario">
                         </div>
                         <div class="mb-3">
-                            <label for="loginPassword" class="form-label">Password</label>
-                            <input type="password" id="loginPassword" class="uiregister form-control" placeholder="Password">
+                            <label for="login-password" class="form-label">Contraseña</label>
+                            <input type="password" id="login-password" class="ui-register form-control" placeholder="Contraseña">
                         </div>
-                        <button class="next btn btn-primary">Next</button>
+                        <button class="next btn btn-primary">Siguiente</button>
                     </form>
-
                     <!-- Register form -->
                     <form id="tabreg-register" class="formaregistro uiregister" enctype="multipart/form-data" action="procesarregistro.php" method="POST">
                         <h2 class="mb-4">Register</h2>
@@ -103,9 +151,8 @@
             </div>
         </div>
     </section>
-
-	<!-- Modal -->
-	<div class="modal fade" id="registroExitosoModal" tabindex="-1" aria-labelledby="registroExitosoModalLabel" aria-hidden="true">
+    <!-- Modal -->
+    <div class="modal fade" id="registroExitosoModal" tabindex="-1" aria-labelledby="registroExitosoModalLabel" aria-hidden="true">
         <div class="modal-dialog">
             <div class="modal-content">
                 <div class="modal-header">
@@ -123,19 +170,15 @@
             </div>
         </div>
     </div>
-
-	<!-- Proceso de registro - PHP -->
-	<?php
+    <!-- Proceso de registro - PHP -->
+    <?php
     if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $response = array(); // Initialize the response array
-
         $servername = "localhost";
         $username = "root";
         $password = "mysql";
         $dbname = "bdapphospital";
-
         $conn = new mysqli($servername, $username, $password, $dbname);
-
         if ($conn->connect_error) {
             $response["status"] = "error";
             $response["message"] = "Error de conexión: " . $conn->connect_error;
@@ -148,13 +191,10 @@
             $fechaNacimiento = $_POST["registerBirthDate"];
             $password = $_POST["registerPassword"];
             $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
-
             $sql = "INSERT INTO usuarios (cedula, nombre, email, telefono, apellidos, fecha_nacimiento, password) 
                     VALUES ('$cedula', '$nombre', '$email', '$telefono', '$apellidos', '$fechaNacimiento', '$hashedPassword')";
-
             if ($conn->query($sql) === TRUE) {
                 $response["status"] = "success";
-
                 // Muestra el modal de registro exitoso
                 echo '<script>
                         $(document).ready(function() {
@@ -164,7 +204,6 @@
                         }, 2000); // Redirige después de 2 segundos (ajusta este valor según tus necesidades)
                     });
                     </script>';
-
                 // Redirect to the index page upon successful registration
                 header("Location: registros.php");
                 exit(); // Ensure script execution is stopped after redirection
@@ -173,22 +212,24 @@
                 $response["message"] = "Error al registrar: " . $conn->error;
             }
         }
-
         // Output JSON response only if it's an AJAX request
         if (isset($_SERVER['HTTP_X_REQUESTED_WITH']) && strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) === 'xmlhttprequest') {
             echo json_encode($response);
         }
-
         $conn->close();
     } else {
         // Handle the case when the script is accessed directly
         echo "No direct access allowed.";
     }
     ?>
-
-	<!-- Footer -->
-	<?php include "footer.php"; ?>
-
+    <!-- Footer -->
+    <?php include "footer.php"; ?>
+    <!-- JavaScript -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+    <script src="js/principal.js"></script>
 </body>
 
 </html>
